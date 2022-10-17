1220b84b56b20aaa76c0e59e1621b080f61be931
nikitasoni025 <nikitasonii025@gmail.com>
2022-10-17 21:49:51 +0530

    project setup

---
+++ src/Pages/Homepage/homepage.js
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+//  const homepage = () => {
+//   return (
+//     <div>homepage wwwww</div>
+//   )
+// }
+// export default homepage;
+import React, { Component } from 'react'
+
+export default class homepage extends Component {
+  render() {
+    return (
+      <div>homepage</div>
+    )
+  }
+}
+
